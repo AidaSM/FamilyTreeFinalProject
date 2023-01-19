8fd1aaec358b5654335249bf46e92dd767f0569c
AidaSM <119674774+AidaSM@users.noreply.github.com>
2023-01-19 12:16:03 +0200

    second commit

---
+++ ClassFamilyTree/ClassFamilyTree/Class1.cs
@@ -0,0 +1,25 @@
+﻿namespace ClassFamilyTree
+{
+    public class Class1
+    {
+        public int Substract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Devide(int a, int b)
+        {
+            return a / b;
+        }
+
+        public int Power(int a)
+        {
+            return a * a;
+        }
+    }
+}
