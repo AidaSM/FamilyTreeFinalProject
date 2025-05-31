4ef4c00062ea8830212ad540d88a7c92fa5b834e
AidaSM <119674774+AidaSM@users.noreply.github.com>
2025-06-01 02:09:14 +0300

    update test file

--- ClassFamilyTree/ClassFamilyTree.Tests/Test1.cs
+++ ClassFamilyTree/ClassFamilyTree.Tests/Test1.cs
@@ -1,45 +1,41 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ClassFamilyTree;
-using DotNetOpenAuth.OpenId.Extensions.SimpleRegistration;
 using System;
+using DotNetOpenAuth.OpenId.Extensions.SimpleRegistration;
 
 namespace ClassFamilyTree.Tests
 {
     [TestClass]
-    public sealed class Test1
+    public class PersonTests
     {
-        [TestClass]
-        public class PersonTests
+        [TestMethod]
+        public void DefaultConstructor_ShouldInitializeWithDefaults()
         {
-            [TestMethod]
-            public void DefaultConstructor_ShouldInitializeWithDefaults()
-            {
-                var person = new Person();
+            var person = new Person();
 
-                Assert.AreEqual("none", person.Name);
-                Assert.AreEqual(default(DateTime), person.DateOfBirth);
-                Assert.AreEqual(Gender.Male, person.Gender);
-                Assert.AreEqual("none", person.Occupation);
-                Assert.AreEqual(0, person.NumberOfChildren);
-            }
+            Assert.AreEqual("none", person.Name);
+            Assert.AreEqual(default(DateTime), person.DateOfBirth);
+            Assert.AreEqual(Gender.Male, person.Gender);
+            Assert.AreEqual("none", person.Occupation);
+            Assert.AreEqual(0, person.NumberOfChildren);
+        }
 
-            [TestMethod]
-            public void ParameterizedConstructor_ShouldAssignAllPropertiesCorrectly()
-            {
-                var expectedName = "Alice";
-                var expectedDob = new DateTime(1990, 1, 1);
-                var expectedGender = Gender.Female;
-                var expectedOccupation = "Engineer";
-                var expectedChildren = 2;
+        [TestMethod]
+        public void ParameterizedConstructor_ShouldAssignAllPropertiesCorrectly()
+        {
+            var expectedName = "Alice";
+            var expectedDob = new DateTime(1990, 1, 1);
+            var expectedGender = Gender.Female;
+            var expectedOccupation = "Engineer";
+            var expectedChildren = 2;
 
-                var person = new Person(expectedName, expectedDob, expectedGender, expectedOccupation, expectedChildren);
+            var person = new Person(expectedName, expectedDob, expectedGender, expectedOccupation, expectedChildren);
 
-                Assert.AreEqual(expectedName, person.Name);
-                Assert.AreEqual(expectedDob, person.DateOfBirth);
-                Assert.AreEqual(expectedGender, person.Gender);
-                Assert.AreEqual(expectedOccupation, person.Occupation);
-                Assert.AreEqual(expectedChildren, person.NumberOfChildren);
-            }
+            Assert.AreEqual(expectedName, person.Name);
+            Assert.AreEqual(expectedDob, person.DateOfBirth);
+            Assert.AreEqual(expectedGender, person.Gender);
+            Assert.AreEqual(expectedOccupation, person.Occupation);
+            Assert.AreEqual(expectedChildren, person.NumberOfChildren);
         }
     }
 }
