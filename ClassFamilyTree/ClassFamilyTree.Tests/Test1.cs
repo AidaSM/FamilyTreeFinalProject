58cd5f6276fa9c5e75e2bf128860127195d2b81e
AidaSM <119674774+AidaSM@users.noreply.github.com>
2025-06-01 02:00:28 +0300

    Updates

---
+++ ClassFamilyTree/ClassFamilyTree.Tests/Test1.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassFamilyTree;
+using DotNetOpenAuth.OpenId.Extensions.SimpleRegistration;
+using System;
+
+namespace ClassFamilyTree.Tests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestClass]
+        public class PersonTests
+        {
+            [TestMethod]
+            public void DefaultConstructor_ShouldInitializeWithDefaults()
+            {
+                var person = new Person();
+
+                Assert.AreEqual("none", person.Name);
+                Assert.AreEqual(default(DateTime), person.DateOfBirth);
+                Assert.AreEqual(Gender.Male, person.Gender);
+                Assert.AreEqual("none", person.Occupation);
+                Assert.AreEqual(0, person.NumberOfChildren);
+            }
+
+            [TestMethod]
+            public void ParameterizedConstructor_ShouldAssignAllPropertiesCorrectly()
+            {
+                var expectedName = "Alice";
+                var expectedDob = new DateTime(1990, 1, 1);
+                var expectedGender = Gender.Female;
+                var expectedOccupation = "Engineer";
+                var expectedChildren = 2;
+
+                var person = new Person(expectedName, expectedDob, expectedGender, expectedOccupation, expectedChildren);
+
+                Assert.AreEqual(expectedName, person.Name);
+                Assert.AreEqual(expectedDob, person.DateOfBirth);
+                Assert.AreEqual(expectedGender, person.Gender);
+                Assert.AreEqual(expectedOccupation, person.Occupation);
+                Assert.AreEqual(expectedChildren, person.NumberOfChildren);
+            }
+        }
+    }
+}
--- FamilyTree/PictureNode.cs
+++ FamilyTree/PictureNode.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Drawing;
 using System.Windows.Forms;
 using FamilyTree;
+using ClassFamilyTree;
 
 namespace FamilyTree
 {
